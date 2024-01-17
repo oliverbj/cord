58a15b82c01d2b6acaf58ed03bca9044d51d99c1
oliverbj <oliverbj@users.noreply.github.com>
2024-01-17 13:40:15 +0000

    Fix styling

--- src/Cord.php
+++ src/Cord.php
@@ -148,17 +148,17 @@ class Cord
 
         return $this;
     }
-    
+
     /**
-    * Add criteriaGroup to the Organization() method.
-    */
+     * Add criteriaGroup to the Organization() method.
+     */
     public function criteriaGroup(array $criteria, string $type = 'Key'): self
     {
 
         if ($this->requestType !== RequestType::NativeOrganizationRetrieval) {
             throw new \Exception('You must call organization() method before calling the criteraGroup() method.');
         }
-        
+
         $criteriaGroup = [
             'CriteriaGroup' => [
                 '_attributes' => ['Type' => $type],

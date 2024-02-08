3d6784fc3ec8a6c6547c7acec2d82dedb16e8225
Oliver Busk Jensen <oliverbusk@hotmail.com>
2024-02-08 15:02:44 +0100

    Create NativeOrganizationUpdate.php

---
+++ src/Requests/NativeOrganizationUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Oliverbj\Cord\Requests;
+
+class NativeOrganizationUpdate extends NativeRequest
+{
+    public function schema(): array
+    {
+        $address = $this->cord->address;
+
+        return $this->defineSchema(
+            address: $address
+
+        );
+    }
+
+    private function defineSchema(array $address): array
+    {
+        $schema = [
+            'Body' => [
+                'Organization' => [],
+            ],
+        ];
+
+        //Push in addresses (if any)
+        if (! empty($address)) {
+            array_push($schema['Body']['Organization'], $address);
+        }
+
+        return $schema;
+    }
+}
